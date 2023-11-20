8823d7ba19ca383a3ae030ac4da8556506496f42
Giacomo Romanini <g.romanini@lunapartner.it>
2023-11-20 19:35:00 +0200

    task three updated

--- task3/rbf.m
+++ task3/rbf.m
@@ -1,14 +1,30 @@
 
-x = 0.1: 1/22: 1;
+start = 0.0;
+x = start : 1/22: 1;
 
 d = ((1 + 0.6 * sin (2 * pi * x / 0.7)) + 0.3 * sin (2 * pi * x)) / 2;
 
 
-c1 = 0.19;
-r1 = 0.15;
+maxs = findMaxs(d);
+centers = findCenters(maxs,d,start);
 
-c2 = 0.92;
-r2 = 0.17;
+%c1 = 0.19;
+xc1 = centers(1);
+%r1 = 0.15;
+range = 1:10;
+things1 = findRadius(d(range),xc1, x(range));
+yc1 = things1(1);
+r1 = things1(2);
+
+%c2 = 0.92;
+xc2 = centers(2);
+
+%r2=0.17;
+range = 14:20;
+things2 = findRadius(d(range),xc2, x(range));
+yc2 = things2(1);
+
+r2 = things2(2);
 
 w1 = rand(1);
 w2 = rand(1);
@@ -19,8 +35,8 @@ eta = 0.7;
 
 for j = 1 : 1000000
     for i = 1 : length(x)
-        v1 = exp(-(x(i)- c1 )^2/(2* r1 ^2));
-        v2 = exp(-(x(i)- c2 )^2/(2* r2 ^2));
+        v1 = exp(-(x(i)- xc1 )^2/(2* r1 ^2));
+        v2 = exp(-(x(i)- xc2 )^2/(2* r2 ^2));
 
         y = w1 * v1 + w2 * v2 + b;
 
@@ -33,12 +49,77 @@ for j = 1 : 1000000
     end
 end
 
-Y = zeros(1,length(x));
-for i = 1 : length(x)
-    v1 = exp(-(x(i)- c1 )^2/(2* r1 ^2));
-    v2 = exp(-(x(i)- c2 )^2/(2* r2 ^2));
+
+
+X = start : 1/200: 1;
+
+
+
+Y = zeros(1,length(X));
+for i = 1 : length(X)
+    v1 = exp(-(X(i)- xc1 )^2/(2* r1 ^2));
+    v2 = exp(-(X(i)- xc2 )^2/(2* r2 ^2));
 
     Y(i) = w1 * v1 + w2 * v2 + b;
 end
 
-plot(x, Y, "r", x, d,"b");
+
+hold on
+plot(xc1,yc1, 'r*');
+plot(xc2,yc2, 'r*');
+plot(X, Y, "r", x, d,"b");
+hold off
+
+
+
+function maximums = findMaxs(f)
+m1 = f(1);
+m2 = f(length(f));
+
+for i = 2 : length(f)-1
+    if(f(i)>f(i-1) && f(i)>f(i+1)) %maximum
+        if(f(i)>=m1)
+            m2 = m1;
+            m1 = f(i);
+        else
+            m2 = f(i);
+        end
+    end
+
+end
+
+maximums(1) = m1;
+maximums(2) = m2;
+
+end
+
+function centers = findCenters(maxs,f,start)
+
+centers(1) = (find(f == maxs(1))-1) * 1/22 + start;
+centers(2) = (find(f == maxs(2))-1) * 1/22 + start;
+end
+
+
+function radius = findRadius(f,center, x)
+l = length(f);
+minimumAverage = 1;
+
+for ycenter = 0.1: 1/40: 1
+   
+    currentCenter = [center, ycenter];
+    total = 0;
+    for j = 1 : l
+        currentFunctionValue = [x(j), f(j)];
+        d = norm(currentCenter-currentFunctionValue);
+        total = d + total;
+    end
+    average = total/l;
+    if(average < minimumAverage)
+        minimumAverage = average;
+        besty = ycenter;
+    end
+    
+end
+radius = [besty,minimumAverage];
+end
+
---
+++ task4/vaizdo_atpazinimas.m
@@ -0,0 +1,148 @@
+close all
+clear all
+clc
+%% read the image with hand-written characters
+pavadinimas = 'train_data.png';
+pozymiai_tinklo_mokymui = pozymiai_raidems_atpazinti(pavadinimas, 9);
+
+%% Development of character recognizer
+% take the features from cell-type variable and save into a matrix-type variable
+P = cell2mat(pozymiai_tinklo_mokymui);
+% create the matrices of correct answers for each line (number of matrices = number of symbol lines)
+T = [eye(8), eye(8), eye(8), eye(8), eye(8), eye(8), eye(8), eye(8), eye(8)];
+% create an RBF network for classification with 13 neurons, and sigma = 1
+tinklas = newrb(P,T,0,1,13);
+
+%% Test of the network (recognizer)
+% estimate output of the network for unknown symbols (row, that were not used during training)
+P2 = P(:,12:22);
+Y2 = sim(tinklas, P2);
+% find which neural network output gives maximum value
+[a2, b2] = max(Y2);
+%% Visualize result
+% calculate the total number of symbols in the row
+raidziu_sk = size(P2,2);
+% we will save the result in variable 'atsakymas'
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+        case 1
+            % the symbol here should be the same as written first symbol in your image
+            atsakymas = [atsakymas, 'A'];
+        case 2
+            atsakymas = [atsakymas, 'B'];
+        case 3
+            atsakymas = [atsakymas, 'C'];
+        case 4
+            atsakymas = [atsakymas, 'D'];
+        case 5
+            atsakymas = [atsakymas, 'E'];
+        case 6
+            atsakymas = [atsakymas, 'F'];
+        case 7
+            atsakymas = [atsakymas, 'G'];
+        case 8
+            atsakymas = [atsakymas, 'H'];
+        case 9
+            atsakymas = [atsakymas, 'I'];
+        case 10
+            atsakymas = [atsakymas, 'K'];
+        case 11
+            atsakymas = [atsakymas, 'J'];
+    end
+end
+% show the result in command window
+disp(atsakymas)
+% % figure(7), text(0.1,0.5,atsakymas,'FontSize',38)
+%% Extract features of the test image
+pavadinimas = 'test_kada.png';
+pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
+
+
+%% Perform letter/symbol recognition
+% features from cell-variable are stored to matrix-variable
+P2 = cell2mat(pozymiai_patikrai);
+% estimating neuran network output for newly estimated features
+Y2 = sim(tinklas, P2);
+% searching which output gives maximum value
+[a2, b2] = max(Y2);
+%% Visualization of result
+% calculating number of symbols - number of columns
+raidziu_sk = size(P2,2);
+% rezultatà saugosime kintamajame 'atsakymas'
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+        case 1
+            atsakymas = [atsakymas, 'A'];
+        case 2
+            atsakymas = [atsakymas, 'B'];
+        case 3
+            atsakymas = [atsakymas, 'C'];
+        case 4
+            atsakymas = [atsakymas, 'D'];
+        case 5
+            atsakymas = [atsakymas, 'E'];
+        case 6
+            atsakymas = [atsakymas, 'F'];
+        case 7
+            atsakymas = [atsakymas, 'G'];
+        case 8
+            atsakymas = [atsakymas, 'H'];
+        case 9
+            atsakymas = [atsakymas, 'I'];
+        case 10
+            atsakymas = [atsakymas, 'K'];
+        case 11
+            atsakymas = [atsakymas, 'J'];
+    end
+end
+
+% disp(atsakymas)
+figure(8), text(0.1,0.5,atsakymas,'FontSize',38), axis off
+%% extract features for next/another test image
+pavadinimas = 'test_fikcija.png';
+pozymiai_patikrai = pozymiai_raidems_atpazinti(pavadinimas, 1);
+
+%% Raidþiø atpaþinimas
+% poþymiai ið celiø masyvo perkeliami á matricà
+P2 = cell2mat(pozymiai_patikrai);
+% skaièiuojamas tinklo iðëjimas neþinomiems poþymiams
+Y2 = sim(tinklas, P2);
+% ieðkoma, kuriame iðëjime gauta didþiausia reikðmë
+[a2, b2] = max(Y2);
+%% Rezultato atvaizdavimas
+% apskaièiuosime raidþiø skaièiø - poþymiø P2 stulpeliø skaièiø
+raidziu_sk = size(P2,2);
+% rezultatà saugosime kintamajame 'atsakymas'
+atsakymas = [];
+for k = 1:raidziu_sk
+    switch b2(k)
+        case 1
+            atsakymas = [atsakymas, 'A'];
+        case 2
+            atsakymas = [atsakymas, 'B'];
+        case 3
+            atsakymas = [atsakymas, 'C'];
+        case 4
+            atsakymas = [atsakymas, 'D'];
+        case 5
+            atsakymas = [atsakymas, 'E'];
+        case 6
+            atsakymas = [atsakymas, 'F'];
+        case 7
+            atsakymas = [atsakymas, 'G'];
+        case 8
+            atsakymas = [atsakymas, 'H'];
+        case 9
+            atsakymas = [atsakymas, 'I'];
+        case 10
+            atsakymas = [atsakymas, 'K'];
+        case 11
+            atsakymas = [atsakymas, 'J'];
+    end
+end
+% pateikime rezultatà komandiniame lange
+% disp(atsakymas)
+figure(9), text(0.1,0.5,atsakymas,'FontSize',38), axis off
+
